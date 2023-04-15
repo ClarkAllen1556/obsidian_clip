35ef3e5024b3d51af49cb0ac324cd79fc0873782
AC1556 <allenclark@u.boisestate.edu>
2023-04-16 00:39:06 +0900

    Fixing error that was preventing reading of clipboard and opening of Obsidian.

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -2,13 +2,20 @@ browser.pageAction.onClicked.addListener(clipPage);
 
 async function clipPage() {
   const { clipper_opts_vault_name: vaultName } = await browser.storage.local.get('clipper_opts_vault_name');
+
+  if (!vaultName) {
+    return;
+  }
+
   const obsidianURI = `obsidian://advanced-uri?vault=${vaultName}&commandname=ReadItLater%3A%20Save%20clipboard`
 
   browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
     const { url } = tabs[0];
 
     navigator.clipboard.writeText(url).then(() => {
-      window.location.assign(obsidianURI, '_blank')
+      browser.tabs.create({
+        url: obsidianURI
+      })
     })
   }, console.error);
 }
