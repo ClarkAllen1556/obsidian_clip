ea354af87c4f5b5d0d33350876941b234e7523e9
AC1556 <allenclark@u.boisestate.edu>
2023-04-16 00:39:47 +0900

    Saved vault name will now be shown in options page

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,6 +1,12 @@
 document.getElementById('clipper_options')
   .addEventListener('submit', () => saveOptions());
 
+browser.storage.local.get("clipper_opts_vault_name").then(resp => {
+  const { vaultName, clipper_opts_vault_name } = resp;
+
+  document.getElementById('clipper_opts_vault_name').value = clipper_opts_vault_name;
+})
+
 function saveOptions(){
   const vaultName = document.getElementById('clipper_opts_vault_name').value;
 
